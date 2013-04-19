9199c1ec469af6144b40475ef2722c0c0bba8218
Hugo Leonardo <hugo.txt@gmail.com>
2013-04-18 21:00:51 -0300

    Fixing tests bootstrap;

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ use Arsenal\Misc\Autoloader;
 // autoloading
 require __DIR__.'/../src/Arsenal/Misc/Autoloader.php';
 $loader = new Autoloader;
-$loader->setFileSystemCheck(true);
+$loader->setCheckFileSystem(true);
 $loader->addFolder('src');
 $loader->addFolder('tests');
 $loader->register();
