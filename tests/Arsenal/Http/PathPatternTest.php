53c0b917a6e88c63a6f5fb8a0449fa304cf497bb
Hugo Leonardo <hugo.txt@gmail.com>
2013-05-08 17:15:18 -0300

    Adding tests for PathPattern

---
+++ tests/Arsenal/Http/PathPatternTest.php
@@ -0,0 +1,98 @@
+<?php
+namespace Arsenal\Http;
+
+class PathPatternTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSimple()
+    {
+        $p = new PathPattern('/foo/bar');
+        $this->assertTrue($p->match('/foo/bar'));
+        $this->assertTrue($p->match('/foo/bar/'));
+        $this->assertTrue($p->match('foo/bar'));
+        $this->assertFalse($p->match('/bar/biz'));
+        
+        $p = new PathPattern('/');
+        $this->assertTrue($p->match('/'));
+        $this->assertTrue($p->match(''));
+        $this->assertTrue($p->match('//'));
+        $this->assertFalse($p->match('/foo/bar'));
+    }
+    
+    public function testPlaceholders()
+    {
+        $p = new PathPattern('/foo/bar/{biz}');
+        $this->assertTrue($p->match('/foo/bar/4'));
+        $this->assertTrue($p->match('/foo/bar/triz'));
+        $this->assertTrue($p->match('foo/bar/54-triz'));
+        $this->assertFalse($p->match('/foo/bar'));
+        $this->assertFalse($p->match('/foo/bar/'));
+        $this->assertFalse($p->match('/foo/bar/5/triz'));
+        
+        $p = new PathPattern('{foo}');
+        $this->assertTrue($p->match('bar'));
+        $this->assertTrue($p->match('/bar'));
+        $this->assertTrue($p->match('/3f42a35'));
+        $this->assertFalse($p->match('/foo/bar'));
+        $this->assertFalse($p->match('/'));
+    }
+    
+    public function testOptionalPlaceholders()
+    {
+        $p = new PathPattern('/foo/bar/{biz?}');
+        $this->assertTrue($p->match('/foo/bar'));
+        $this->assertTrue($p->match('/foo/bar/'));
+        $this->assertTrue($p->match('/foo/bar/3'));
+        $this->assertTrue($p->match('/foo/bar/biz'));
+        $this->assertFalse($p->match('/foo'));
+        $this->assertFalse($p->match('/foo/bar/triz/kaz'));
+        $this->assertFalse($p->match('/foo/bar/3/4'));
+        
+        $p = new PathPattern('/foo/bar/{biz?}/kaz');
+        $this->assertTrue($p->match('/foo/bar/biz/kaz'));
+        $this->assertFalse($p->match('/foo/bar'));
+        
+        $p = new PathPattern('/foo/bar/{biz?}/{kaz}/{triz}');
+        $this->assertTrue($p->match('/foo/bar/biz/kaz/triz'));
+        $this->assertFalse($p->match('/foo/bar/kaz/triz'));
+    }
+    
+    public function testRegex()
+    {
+        $p = new PathPattern('~.*\/?(.+\.jpg)');
+        $this->assertTrue($p->match('/foo/bar/image.jpg'));
+        $this->assertTrue($p->match('image.jpg'));
+        $this->assertFalse($p->match('image.gif'));
+        $this->assertFalse($p->match('image.jpg/foo'));
+    }
+    
+    public function testSimpleMatches()
+    {
+        $p = new PathPattern('foo/bar/{biz}/kaz/{tar}/{mil?}');
+        $m = array();
+        $p->match('/foo/bar/biz/kaz/tar', $m);
+        
+        $this->assertTrue(count($m) === 3);
+        $this->assertTrue($m[0] === 'biz');
+        $this->assertTrue($m[1] === 'tar');
+        $this->assertTrue($m[2] === null);
+    }
+    
+    public function testRegexMatches()
+    {
+        $p = new PathPattern('~.*\/(.+\.jpg)');
+        $m = array();
+        $p->match('/foo/bar/image.jpg', $m);
+        
+        $this->assertTrue(count($m) === 1);
+        $this->assertTrue($m[0] === 'image.jpg');
+        
+        
+        $p = new PathPattern('~/foo/bar/(.+)/biz/(.+)');
+        $m = array();
+        $p->match('/foo/bar/triz/biz/kar', $m);
+        
+        $this->assertTrue(count($m) === 2);
+        $this->assertTrue($m[0] === 'triz');
+        $this->assertTrue($m[1] === 'kar');
+    }
+}
