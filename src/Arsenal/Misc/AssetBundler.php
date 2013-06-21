a3409e34fd80ecf6760662ac418f59fee270c918
Hugo Leonardo <hugo.txt@gmail.com>
2013-06-21 10:41:00 -0300

    Adding AssetBundler

---
+++ src/Arsenal/Misc/AssetBundler.php
@@ -0,0 +1,58 @@
+<?php
+namespace Arsenal\Misc;
+
+class AssetBundler
+{
+    private $root = null;
+    private $fluid = true;
+    private $bundles = array();
+    
+    public function __construct($root)
+    {
+        $this->root = realpath($root);
+    }
+    
+    public function setFluid($bool)
+    {
+        $this->fluid = $bool;
+    }
+    
+    public function add($bundle, $file)
+    {
+        if( ! isset($this->bundles[$bundle]))
+            $this->bundles[$bundle] = array();
+        $this->bundles[$bundle][] = $file;
+    }
+    
+    public function getCssTag($bundle, $media = 'all')
+    {
+        return '<link rel="stylesheet" href="'.$this->getBundle($bundle).'" media="'.$media.'">';
+    }
+    
+    public function getJsTag($bundle)
+    {
+        return '<script type="text/javascript" src="'.$this->getBundle($bundle).'"></script>';
+    }
+    
+    public function getBundle($bundle)
+    {
+        if( ! isset($this->bundles[$bundle]))
+            throw new \InvalidArgumentException('Bundle "'.$bundle.'" not found');
+        
+        if($this->fluid)
+            $this->makeBundle($bundle);
+        
+        return $bundle;
+    }
+    
+    private function makeBundle($bundle)
+    {
+        $files = $this->bundles[$bundle];
+        $string = '';
+        $d = DIRECTORY_SEPARATOR;
+        foreach($files as $file)
+            $string .= file_get_contents($this->root.$d.$file)."\n\n";
+        $string = substr($string, 0, -2);
+        file_put_contents($this->root.$d.$bundle, $string);
+    }
+}
