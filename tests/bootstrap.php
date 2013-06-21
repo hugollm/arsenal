524ea181e3401aa5bb3fcac6c9c7960c68be6c36
Hugo Leonardo <hugo.txt@gmail.com>
2013-06-21 00:29:50 -0300

    Updating tests/boostrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,5 +7,6 @@ require __DIR__.'/../src/Arsenal/Misc/Autoloader.php';
 $loader = new Autoloader;
 $loader->setCheckFileSystem(true);
 $loader->addFolder('src');
+$loader->addFolder('src-obsolete');
 $loader->addFolder('tests');
 $loader->register();
